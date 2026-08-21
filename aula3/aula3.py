17888f21c5c34e5e4f00f4cbbc1055ea1576f46e
LauraGuedin05 <lauraguedin6@gmail.com>
2026-08-20 22:18:15 -0300

    Add files via upload

---
+++ aula3/aula3.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def curriculo():
+    informacoes = {
+        "nome": "Henrique Santos Tavares",
+        "email": "henriquestv18@gmail.com",
+        "escola": "Escola Técnica do COTEMIG",
+        "tecnologias": ["MySQL", "C#", "N8N", "Python", "React"]
+    }
+    return render_template('index.html', **informacoes)
+
+@app.route('/cotemig/<nome_escola>')
+def rota_cotemig(nome_escola):
+    return f"<h1>{nome_escola}</h1>"
+
+@app.route('/a')
+def curriculo2():
+    informacoes = {
+        "nome": "Henrique Santos Tavares",
+        "email": "henriquestv18@gmail.com",
+        "escola": "Escola Técnica do COTEMIG",
+        "tecnologias": ["MySQL", "C#", "N8N", "Python", "React"]
+    }
+    return render_template('index.html', **informacoes)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
